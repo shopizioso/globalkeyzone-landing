c399829e834fb387e14edbe4b71d825869c5193f
shopizioso <53453565+shopizioso@users.noreply.github.com>
2026-05-24 17:58:06 +0700

    Replace main content with a red div

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-"use client"
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black flex items-center justify-center text-white">
-
-      <h1 className="text-6xl font-black">
-        TEST BLACK MODE
-      </h1>
-
-    </main>
+    <div
+      style={{
+        background: "red",
+        height: "100vh",
+        width: "100%",
+      }}
+    />
   )
 }
