51a8c1c684ce1869ea24ebd31865ec9a93fafa96
shopizioso <53453565+shopizioso@users.noreply.github.com>
2026-05-24 08:41:40 +0700

    Refactor AICore component with CoreSphere and OrbitControls

--- app/components/AICore.tsx
+++ app/components/AICore.tsx
@@ -1,43 +1,40 @@
-'use client'
+"use client"
 
-import { Canvas } from '@react-three/fiber'
-import { Float, MeshDistortMaterial, Sphere } from '@react-three/drei'
+import { Canvas } from "@react-three/fiber"
+import { Float, OrbitControls } from "@react-three/drei"
 
-function Orb() {
+function CoreSphere() {
   return (
-    <Float speed={2} rotationIntensity={1.5} floatIntensity={2}>
-      <Sphere args={[1, 64, 64]} scale={1.8}>
-        <MeshDistortMaterial
-          color="#00d9ff"
-          distort={0.3}
-          speed={1.8}
-          roughness={0.4}
-          metalness={0.8}
-          opacity={0.35}
-          transparent
+    <Float speed={2} rotationIntensity={2} floatIntensity={3}>
+      <mesh>
+        <sphereGeometry args={[1.5, 64, 64]} />
+        <meshStandardMaterial
+          color="#7dd3fc"
+          emissive="#38bdf8"
+          emissiveIntensity={2}
+          roughness={0.1}
+          metalness={1}
         />
-      </Sphere>
+      </mesh>
     </Float>
-  );
+  )
 }
 
 export default function AICore() {
   return (
-    <div className="absolute inset-0 opacity-40"
-
+    <div className="absolute inset-0 z-0">
       <Canvas camera={{ position: [0, 0, 4] }}>
-
         <ambientLight intensity={1.5} />
+        <directionalLight position={[2, 2, 2]} intensity={2} />
 
-        <directionalLight
-          position={[3, 3, 3]}
-          intensity={2}
+        <CoreSphere />
+
+        <OrbitControls
+          enableZoom={false}
+          autoRotate
+          autoRotateSpeed={2}
         />
-
-        <CoreOrb />
-
       </Canvas>
-
     </div>
   )
 }
